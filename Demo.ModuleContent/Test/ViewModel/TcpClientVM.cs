570d1c638957cc79a167df89f41b17b3f703d492
aaron yang <yangyr.aaron@gmail.com>
2013-11-24 21:58:46 +0800

    add tcp test

--- Demo.ModuleContent/Test/ViewModel/TcpClientVM.cs
+++ Demo.ModuleContent/Test/ViewModel/TcpClientVM.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Demo.Content.Test
@@ -11,12 +13,17 @@ namespace Demo.Content.Test
     [Export]
     public class TcpClientVM : NotificationObject
     {
+        private byte[] _data = new byte[1024];
+        private Socket _server;
+
         public TcpClientVM()
         {
-            this.SendCommond = new DelegateCommand<object>(this.SendMessage);
+            this.ConnectCommand = new DelegateCommand<object>(o=>this.Connect());
+            this.CloseCommand = new DelegateCommand<object>(o=>this.Close());
+            this.SendCommand = new DelegateCommand<object>(o=>this.SendMessage());
         }
 
-        private string _ip;
+        private string _ip = "127.0.0.1";
         public string Ip
         {
             get { return _ip; }
@@ -34,19 +41,73 @@ namespace Demo.Content.Test
             }
         }
 
-        private string _message;
-        public string Message
+        private string _response = "No Response!";
+        public string Response
         {
-            get { return _message; }
-            set { _message = value;
-            //this.RaisePropertyChanged(() => this.Message);
+            get { return _response; }
+            set {
+                if (_response == value)
+                    return;
+                _response = value;
+            this.RaisePropertyChanged(() => this.Response);
             }
         }
 
-        DelegateCommand<object> SendCommond { get; set; }
-
-        private void SendMessage(object obj) { 
+        private string _inputMessage;
+        public string InputMessage
+        {
+            get { return _inputMessage; }
+            set { _inputMessage = value; }
+        }
         
+
+        private string _log = "No log!";
+        public string Log
+        {
+            get { return _log; }
+            set {
+                if (_log == value)
+                    return;
+                _log = value;
+            this.RaisePropertyChanged(() => this.Log);
+            }
+        }
+
+        public DelegateCommand<object> SendCommand { get; set; }
+        public DelegateCommand<object> ConnectCommand { get; set; }
+        public DelegateCommand<object> CloseCommand { get; set; }
+
+        private void Connect() {
+            IPEndPoint end = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                _server.Connect(end);
+
+                int recv = this._server.Receive(_data);
+                string message = Encoding.ASCII.GetString(_data, 0, recv);
+                this.Response = message;
+            }
+            catch (SocketException e)
+            {
+                this.Log = "unable to connect to server \r\n" + e.ToString();
+                return;
+            }
+            this.Log = "the connection has been established!\r\n";
+        }
+
+        private void Close() {
+            this._server.Shutdown(SocketShutdown.Both);
+            this._server.Close();
+
+            this.Log = "the connection has been closed!\r\n";
+        }
+        private void SendMessage() { 
+            this._server.Send(Encoding.ASCII.GetBytes(this.InputMessage));
+            _data = new byte[1024];
+            int recv = _server.Receive(_data);
+            this.Response = Encoding.ASCII.GetString(_data, 0, recv);
         }
 
     }
